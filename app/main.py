cbdb49867b7989adab47562c38f2a4873f55ae26
JAVIER ANTONIO CUNDAPI TOLEDO <223222@ids.upchiapas.edu.mx>
2024-09-25 23:09:01 -0600

    ss

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from app.api.routes import router 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
+
+
 
 app.include_router(router,prefix="/api")
 
